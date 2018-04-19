4eeb65b96e62ff195673cf7c3c279ec319f05047
Jason O'Donnell <2160810+jasonodonnell@users.noreply.github.com>
2018-04-19 11:54:42 -0400

    Fix startup script

--- setup.sh
+++ setup.sh
@@ -10,7 +10,5 @@ fi
 
 ssh-keygen -b 2048 -t rsa -f ./keys/id_rsa -q -N ""
 chmod 400 ./keys/id_rsa*
-cp ./keys/id_rsa.pub ./roles/common/files/authorized_keys
-cp ./keys/id_rsa ./roles/common/files/id_rsa
 
 exit 0
